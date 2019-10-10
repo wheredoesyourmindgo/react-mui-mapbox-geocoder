d3b75444e2bb08f25212529ca097a1e96faf747b
pcwa-ahendricks <ahendricks@pcwa.net>
2019-10-10 15:09:50 -0700

    Demo updated.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,18 @@
 import React, {useState, useCallback} from 'react';
 import MapGL, {NavigationControl, FlyToInterpolator} from 'react-map-gl';
 import {easeCubic} from 'd3-ease';
+import {ThemeProvider} from '@material-ui/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 import MatGeocoder from 'react-mui-mapbox-geocoder';
 // import for use with developing component from copy of source.
 // import MatGeocoder from './lib/MatGeocoder';
 // import for use with developing component from local copy of dist.
 // import MatGeocoder from './lib';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import {withStyles} from '@material-ui/core/styles';
+// import {makeStyles} from '@material-ui/styles';
 import './App.css';
+
+const theme = createMuiTheme();
 
 const geocoderApiOptions = {
   country: 'us'
@@ -16,20 +20,20 @@ const geocoderApiOptions = {
   // bbox: [-123.8501, 38.08, -117.5604, 39.8735]
 };
 
-const styles = {
-  input: {
-    // backgroundColor: 'white',
-    borderRadius: 4,
-    '&$cssFocused $notchedOutline': {
-      borderColor: 'green'
-    }
-  },
-  notchedOutline: {},
-  cssFocused: {},
-  textField: {
-    // backgroundColor: 'blue',
-  }
-};
+// const useStyles = makeStyles({
+//   input: {
+//     // backgroundColor: 'white',
+//     borderRadius: 4,
+//     '&$cssFocused $notchedOutline': {
+//       borderColor: 'green'
+//     }
+//   },
+//   notchedOutline: {},
+//   cssFocused: {},
+//   textField: {
+//     // backgroundColor: 'blue',
+//   }
+// });
 
 const initialViewport = {
   width: window.innerWidth || 400,
@@ -39,7 +43,8 @@ const initialViewport = {
   zoom: 8
 };
 
-const Demo = ({classes}) => {
+const Demo = () => {
+  // const classes = useStyles();
   const [viewport, setViewport] = useState(initialViewport);
 
   const _onViewportChange = useCallback((viewport) => {
@@ -63,55 +68,57 @@ const Demo = ({classes}) => {
   );
 
   return (
-    <div className="App">
-      <MapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        onViewportChange={_onViewportChange}
-      >
-        <div className="navControls">
-          <NavigationControl onViewportChange={_onViewportChange} />
-        </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          onViewportChange={_onViewportChange}
+        >
+          <div className="navControls">
+            <NavigationControl onViewportChange={_onViewportChange} />
+          </div>
 
-        <div className="geocoder">
-          <MatGeocoder
-            focusOnMount
-            inputPlaceholder="Search Address"
-            inputValue="Foobar"
-            accessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-            onSelect={(result) => _handleGeocoderSelect(result)}
-            showLoader={true}
-            {...geocoderApiOptions}
-            inputPaperProps={{square: true}}
-            suggestionsPaperProps={{square: true}}
-            // inputTextFieldProps={{
-            //   variant: 'outlined',
-            //   fullWidth: true,
-            //   classes: {
-            //     root: classes.textField,
-            //     notchedOutline: classes.notchedOutline,
-            //     focused: classes.focused
-            //   }
-            // }}
-            // inputTextFieldProps={{
-            //   variant: 'outlined',
-            //   fullWidth: false,
-            //   classes: {
-            //     root: classes.textField
-            //   }
-            // }}
-            // inputClasses={{root: classes.input}}
-            // inputClasses={{
-            //   root: classes.input,
-            //   notchedOutline: classes.notchedOutline,
-            //   focused: classes.cssFocused
-            // }}
-            // showInputContainer={false}
-          />
-        </div>
-      </MapGL>
-    </div>
+          <div className="geocoder">
+            <MatGeocoder
+              focusOnMount
+              inputPlaceholder="Search Address"
+              inputValue="Foobar"
+              accessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+              onSelect={(result) => _handleGeocoderSelect(result)}
+              showLoader={true}
+              {...geocoderApiOptions}
+              inputPaperProps={{square: true}}
+              suggestionsPaperProps={{square: true}}
+              // inputTextFieldProps={{
+              //   variant: 'outlined',
+              //   fullWidth: true,
+              //   classes: {
+              //     root: classes.textField,
+              //     notchedOutline: classes.notchedOutline,
+              //     focused: classes.focused
+              //   }
+              // }}
+              // inputTextFieldProps={{
+              //   variant: 'outlined',
+              //   fullWidth: false,
+              //   classes: {
+              //     root: classes.textField
+              //   }
+              // }}
+              // inputClasses={{root: classes.input}}
+              // inputClasses={{
+              //   root: classes.input,
+              //   notchedOutline: classes.notchedOutline,
+              //   focused: classes.cssFocused
+              // }}
+              // showInputContainer={false}
+            />
+          </div>
+        </MapGL>
+      </div>
+    </ThemeProvider>
   );
 };
 
-export default withStyles(styles)(Demo);
+export default Demo;
